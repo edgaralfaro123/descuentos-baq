3593dc8d69b6c4feca49886e69e48e05ff07703a
George Alvarado <georgealvarado@outlook.com>
2020-05-21 14:31:38 -0500

    Agregado el formulario de busqueda de productos

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
 //import React from 'react';
-import React from 'react';
+import React, { useState, useEffect,useRef  } from 'react';
 import './App.css';
 import Product from "./components/products";
 
 const App = () => {
+  const productRef = useRef();
+  const [searchitems, setSearch] = useState([]);
 
-  // const buscarArticulos = async () => {
-  //
-  //   let url = `https://cuentasporcobrar.herokuapp.com/descuentos/producto/valor`;
-  //   fetch(url)
-  //   .then((response) => {
-  //     return response.json();
-  //   })
-  //   .then((result)=> {
-  //
-  //
-  //     console.log(result);
-  //   })
-  //   .catch((err)=>{
-  //     console.error(err);
-  //   })
-  // }
+  const buscarArticulos = async () => {
+    let value=productRef.current.value;
+    let url = `https://cuentasporcobrar.herokuapp.com/descuentos/producto/${value}`;
+    fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((result)=> {
+
+      setSearch(result);
+      console.log(result);
+    })
+    .catch((err)=>{
+      console.error(err);
+    })
+  }
 
   return(
-    <div>
+    <div className='row'>
+      <div className='col-sm-12 pading'>
+        <center><a className="display-4"><strong> En cuanto me queda?</strong></a></center>
+        <input type="text" className="form-control" ref={productRef} aria-describedby="emailHelp" placeholder="Que producto deseas comprar hoy?"></input>
+        <button className="btn btn-danger" onClick={buscarArticulos}>Buscar</button>
+    </div>
+      <div className="pt-5 justify-content-center justify-content-between">
+        <Product items={searchitems}></Product>
+      </div>
+    </div>
 
-      <Product></Product>
-  </div>
+
   )
 
 
