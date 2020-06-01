386809604fa880da000991ecc26257a1e9b4a1fa
Edgar Alfaro <soportecloudnets@outlook.es>
2020-06-01 14:35:39 -0500

    Organizo boton de busqueda

--- src/components/header.js
+++ src/components/header.js
@@ -16,10 +16,20 @@ const Header = (props) =>{
     } 
 
     return(
-        <div className='col-sm-12 pading'>
+
+        <div class="row">
+            <div className='col-sm-12 justify-content-center'>
             <center><a className="display-4"><strong> En cuanto me queda?</strong></a></center>
-            <input type="text" className="form-control" value={searchBar} onChange={inputchange} aria-describedby="emailHelp" placeholder="Que producto deseas comprar hoy?"></input>
-            <button className="btn btn-danger" onClick={()=>buscararticulo(searchBar)}>Buscar</button>
+
+            </div>
+            <div className='col-sm-2'></div>
+            <div className='col-sm-7 pading'>
+                <input type="text" className="form-control" value={searchBar} onChange={inputchange} aria-describedby="emailHelp" placeholder="Que producto deseas comprar hoy?"></input>
+            </div>
+            <div className='col-sm-1 pading'>
+                <button className="btn btn-danger col-sm-12" onClick={()=>buscararticulo(searchBar)}>Buscar</button>
+            </div>
+            <div className='col-sm-2'></div>
         </div>
         
     )
